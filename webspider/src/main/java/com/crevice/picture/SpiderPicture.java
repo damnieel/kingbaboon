10bd7e73b2506b39cf669d95ef352826461a5cad
damnieel <331712034@qq.com>
2018-12-06 18:12:51 +0800

    1

--- webspider/src/main/java/com/crevice/picture/SpiderPicture.java
+++ webspider/src/main/java/com/crevice/picture/SpiderPicture.java
@@ -15,6 +15,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.crevice.util.SpiderUtil;
+
 /**
  * 通过HTML解析实现图片批量下载
  * @author DorraChen
@@ -80,30 +82,10 @@ public class SpiderPicture {
         // 截取图片的名称
         String fileName = imageUrl.substring(imageUrl.lastIndexOf("/"));
         
-        //创建文件的目录结构
-        File files = new File(filePath);
-        if(!files.exists()){// 判断文件夹是否存在，如果不存在就创建一个文件夹
-            files.mkdirs();
-        }
-        try {
-            URL url = new URL(imageUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36");
-            connection.setRequestProperty("referer", "http://i.meizitu.net");
-            InputStream is = connection.getInputStream();
-            // 创建文件
-            File file = new File(filePath+fileName);
-            FileOutputStream out = new FileOutputStream(file);
-            int i = 0;
-            while((i = is.read()) != -1){
-                out.write(i);
-            }
-            is.close();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        SpiderUtil.downloadFile(filePath, fileName, imageUrl);
     }
+
+	
     
     //执行测试程序代码
     public static void main(String[] args) throws IOException {
@@ -113,23 +95,28 @@ public class SpiderPicture {
         System.out.println("编码方式："+encoding);
         String filePath = "C:\\Users\\CM20180419\\Desktop\\beauty";
         System.out.println("下载到电脑的位置："+filePath);
-       /* String htmlResource = getHtmlResourceByUrl(url, encoding);*/
-        // System.out.println(htmlResource);
         // 解析网页源代码
-       /* Document document = Jsoup.parse(htmlResource);*/
-        Document document = Jsoup.connect(url).timeout(6000).userAgent("Mozilla\" to \"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0)").get();
+        /*Document document = Jsoup.parse(htmlResource);*/
+        Document document = Jsoup.connect(url)
+					        	 .timeout(6000)
+					        	 .userAgent("Mozilla\" to \"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0)")
+					        	 .get();
         // 获取所有图片的地址
         Elements elements = document.getElementsByTag("img");
         
         for(Element element : elements){
-            String imgSrc = element.attr("data-original");
-            if (!"".equals(imgSrc)) {
+            String imageUrl = element.attr("data-original");
+            if (!"".equals(imageUrl)) {
                 // 判断imgSrc是否为空且是否以"http://"开头\
-            	if(!(imgSrc.startsWith("http://") || imgSrc.startsWith("https://"))){
-            		imgSrc = "http:"+imgSrc;
+            	if(!(imageUrl.startsWith("http://") || imageUrl.startsWith("https://"))){
+            		imageUrl = "http:"+imageUrl;
             	}
-                System.out.println("正在下载的图片的地址：" + imgSrc);
-                downImages(filePath, imgSrc);
+                System.out.println("正在下载的图片的地址：" + imageUrl);
+                
+                // 截取图片的名称
+                String fileName = imageUrl.substring(imageUrl.lastIndexOf("/"));
+                
+                SpiderUtil.downloadFile(filePath, fileName, imageUrl);
             }
         }
         System.out.println("-------------------------下载完毕！----------------------------");
--- webspider/src/main/java/com/crevice/util/SpiderUtil.java
+++ webspider/src/main/java/com/crevice/util/SpiderUtil.java
@@ -1,8 +1,73 @@
 package com.crevice.util;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 
 public class SpiderUtil {
+	/**
+	 * xiaohui
+	 * 2018/12/6
+	 * @param filePath 文件所要存放的路径
+	 * @param fileName 文件的所要取得名称
+	 * @param sourceUrl 源文件url
+	 */
+	public static void downloadFile(String filePath,String fileName,String sourceUrl) {
+		//创建文件的目录结构
+        File files = new File(filePath);
+        if(!files.exists()){// 判断文件夹是否存在，如果不存在就创建一个文件夹
+            files.mkdirs();
+        }
+        try {
+            URL url = new URL(sourceUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36");
+            connection.setRequestProperty("referer", "http://i.meizitu.net");
+            InputStream is = connection.getInputStream();
+            // 创建文件
+            File file = new File(filePath+fileName);
+            FileOutputStream out = new FileOutputStream(file);
+            int i = 0;
+            while((i = is.read()) != -1){
+                out.write(i);
+            }
+            is.close();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	public static void main(String[] args) throws Exception {
 		String html = HttpRequest.get("https://www.mzitu.com", new HashMap<String, String>(),"gb2312");
