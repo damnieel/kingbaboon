14c55a2cd1c85b7d42784f1b72d06e27a0260528
damnieel <331712034@qq.com>
2018-12-06 18:15:07 +0800

    1

--- webspider/src/main/java/com/crevice/picture/SpiderPicture.java
+++ webspider/src/main/java/com/crevice/picture/SpiderPicture.java
@@ -1,12 +1,8 @@
 package com.crevice.picture;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
